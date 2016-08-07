043932bd780d7ab6ac4882ae96b6095d4c585941
Tafarel Mello <tafarel.mello@gmail.com>
2016-08-07 03:58:30 -0300

    ...
    ...

---
+++ SalvadoImg/Salvado-img/View/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImg = new System.Windows.Forms.PictureBox();
+            this.btnclick = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImg
+            // 
+            this.pbImg.Location = new System.Drawing.Point(12, 12);
+            this.pbImg.Name = "pbImg";
+            this.pbImg.Size = new System.Drawing.Size(401, 246);
+            this.pbImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImg.TabIndex = 0;
+            this.pbImg.TabStop = false;
+            // 
+            // btnclick
+            // 
+            this.btnclick.Location = new System.Drawing.Point(11, 264);
+            this.btnclick.Name = "btnclick";
+            this.btnclick.Size = new System.Drawing.Size(128, 34);
+            this.btnclick.TabIndex = 1;
+            this.btnclick.Text = "Selectionar img";
+            this.btnclick.UseVisualStyleBackColor = true;
+            this.btnclick.Click += new System.EventHandler(this.btnclick_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "mostrar img";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 417);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnclick);
+            this.Controls.Add(this.pbImg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImg;
+        private System.Windows.Forms.Button btnclick;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SalvadoImg/Salvado-img/View/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+
+namespace View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //exibir a imagem no form
+                string nomeArquivo = openFileDialog1.FileName;
+                Bitmap bitmap = new Bitmap(nomeArquivo);
+                pbImg.Image = bitmap;
+
+                //
+                MemoryStream ms = new MemoryStream();
+                bitmap.Save(ms, ImageFormat.Bmp);
+                //
+                byte[] foto = ms.ToArray();
+
+                SqlConnection conn = new SqlConnection(@"Data Source=LOCALHOST;Initial Catalog=SalvandoImagem;Integrated Security=True");
+                SqlCommand comand = new SqlCommand("INSERT INTO Cliente_Foto (IdCliente, Foto) VALUES (1, @foto)", conn);
+
+                SqlParameter parameter = new SqlParameter("@foto", SqlDbType.Binary);
+                parameter.Value = foto;
+
+                comand.Parameters.Add(parameter);
+
+                conn.Open();
+                comand.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=LOCALHOST;Initial Catalog=SalvandoImagem;Integrated Security=True");
+            SqlCommand comand = new SqlCommand("SELECT Foto FROM Cliente_Foto WHERE IdCliente = 1", conn);
+
+            conn.Open();
+            SqlDataReader reader = comand.ExecuteReader();
+
+            Image imagem = null;
+            if (reader.Read())
+            {
+                byte[] foto = (byte[])reader["Foto"];
+
+                MemoryStream ms = new MemoryStream(foto);
+                imagem = Image.FromStream(ms);
+            }
+
+            pbImg.Image = imagem;
+
+            conn.Close();
+        }
+    }
+}
